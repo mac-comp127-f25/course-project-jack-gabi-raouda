8c2403ce0fe97f8e5d7de24e114e7012aad1b122
gspalladino <gpalladi@macalester.edu>
2025-11-24 11:24:07 -0600

    Main window skeleton created

--- src/FishingGame.java
+++ src/FishingGame.java
@@ -1,3 +1,89 @@
+import java.awt.Color;
+import java.util.List;
+
+
+import edu.macalester.graphics.CanvasWindow;
+import edu.macalester.graphics.GraphicsObject;
+
 public class FishingGame {
+    private static final int CANVAS_WIDTH = 600;
+    private static final int CANVAS_HEIGHT = 800;
+    private static final double WATER_LEVEL = CANVAS_HEIGHT - 150;
+    private FishingLine fishingLine;
+    private Boat boat;
+    private List<Fish> fish;
+    private boolean lineMoving = false;
     
+
+    public CanvasWindow canvas;
+    
+
+    public FishingGame() {
+        canvas = new CanvasWindow("Fishing!", CANVAS_WIDTH, CANVAS_HEIGHT);
+        canvas.setBackground(Color.BLUE);
+
+        // boat = new boat(CANVAS_WIDTH / 2, CANVAS_HEIGHT - 50);
+        // canvas.add(boat.getGraphicObject());
+        
+        //code that handles keyboard input to move the boat left and right
+    }
+
+    /**
+     * Main method that calls the run method.
+     */
+    public static void main(String[] args){
+        FishingGame game = new FishingGame();
+        game.run();
+    }
+
+    /**
+     * Runs the game.
+     */
+    public void run() {
+        //code
+        
+    }
+
+      /**
+     * Resets the canvas by removing everything, redrawing new fish, and resetting the paddle and ball to default position.
+     */
+    public void resetGame() {
+        // canvas.removeAll();
+        // generateAllFish();
+        // canvas.add(boat.getGraphicObject());
+        // canvas.draw();
+    }
+
+    /**
+     * Creates the fishingLine object in the center of the canvas.
+     */   
+    private void createFishingLine() {
+        //make fishing line ? maybe this can be called in the boat class instead
+    }
+
+
+    /**
+     * Generates the fish list and adds them to the canvas.
+     */
+    public void generateAllFish() {
+        // fishes = Fish.createBricks();
+        // for (Fish fish : fishes) {
+        //     canvas.add(fish.getGraphicObject());
+        // }
+    }
+
+    /**
+     * Checks if the line has hit any fish and pulls them up to the water surface if so.
+     */
+    private void checkIfFishHit() {
+        //code
+    }
+    
+    /**
+     * Helper method to remove the fish from the canvas when it reaches the surface of the water.
+     */
+    private void removeFishAtSurface(GraphicsObject caughtFish) {
+        //code
+    }
+
 }
