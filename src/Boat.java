2d009502e1cbc79fe970d3be1512fe5e9c0729a3
RaoudaBK <rmamaneb@macalester.edu>
2025-12-05 02:28:57 -0600

    Created fishing line and animated it, updated boat with mat and sail for it to be esthetically pleasing and added movement for boat and line

--- src/Boat.java
+++ src/Boat.java
@@ -14,6 +14,7 @@ public class Boat {
     private GraphicsGroup boatGroup;
 
     public Boat(double x, double y){
+        // Body
         boatGroup = new GraphicsGroup(x, y);
         Path body = new Path(Arrays.asList(
             new Point(0, 40),  
@@ -22,37 +23,50 @@ public class Boat {
             new Point(20, 80)
         ));
         body.setFillColor(new Color(255, 0, 0));
-        body.setStrocked(false);
-        boatGroup.add(body);
+        body.setStroked(false);
+        boatGroup.add(body); 
+
+        // Mast
+        Rectangle mast = new Rectangle(80, -50, 8, 90);
+        mast.setFillColor(new Color(120, 80, 40)); 
+        mast.setStroked(false);
+        boatGroup.add(mast);
+
+
+        // Sail
+        Path sail = new Path(Arrays.asList(
+            new Point(88, -50),   
+            new Point(88, 10),    
+            new Point(140, -20)  
+        ));
+
+        sail.setFillColor(new Color(255, 255, 255)); 
+        sail.setStroked(true);
+        boatGroup.add(sail);
+    }
 
     
+    public void move(double mouseX, int canvasWidth) {
+        double newX = mouseX - boatGroup.getWidth() / 2;
 
-    
-    public void mvoeLeft(){
-        boatGroup.moveBy(-5,0);
+        // Keep boat inside the screen
+        if (newX < 0) newX = 0;
+        if (newX + boatGroup.getWidth() > canvasWidth) {
+            newX = canvasWidth - boatGroup.getWidth();
+        }
+
+        boatGroup.setX(newX);
     }
-    public void moveRight(){
-        boatGroup.moveBy(5,0);
-    }
+
     public GraphicsObject getGraphicsObject(){
         return boatGroup;
     }
+
     public double getX(){
         return boatGroup.getX();
     }
     public double getY(){
         return boatGroup.getY();
     }
-
-
-
-
-
-
-
     
-
-    // public Boat(params) {
-    //     //code to create the boat graphic object at the given position
-    // }
 }
--- src/FishingLine.java
+++ src/FishingLine.java
@@ -2,24 +2,36 @@ import edu.macalester.graphics.CanvasWindow;
 import edu.macalester.graphics.Rectangle;
 
 public class FishingLine {
+    private CanvasWindow canvas;
     private Rectangle line;
-    private double startX;
-    private double startY;
+    private double x;
+    private double y;
 
+    private double length = 0;
     private boolean dropping = false;
     private boolean goingUp = false;
 
-    private static final double LineWidth = 3;
-    private static final double MaxLength = 450;
-    private static final double Speed = 3;
+    private static final double LINE_WIDTH = 3;
+    private static final double MAX_LENGTH = 450;
+    private static final double SPEED = 3;
 
     public FishingLine(CanvasWindow canvas, double startX, double startY) {
-        this.startX = startX;
-        this.startY = startY;
-        line = new Rectangle(startX, startY, LineWidth, 0);
+        this.canvas = canvas;
+        this.x = startX;
+        this.y = startY;
+        line = new Rectangle(x, y, LINE_WIDTH, 0);
         line.setFillColor(java.awt.Color.BLACK);
         canvas.add(line);
     }
+
+    // Attach line top to boat whenver it moves
+     public void updatePosition(double boatX, double boatY) {
+        this.x = boatX;
+        this.y = boatY;
+
+        redrawLine();
+    }
+
     public void dropLine() { 
         dropping = true; 
         goingUp = false; 
@@ -29,19 +41,34 @@ public class FishingLine {
     goingUp = true;
     dropping = false;
 }
- public void update() {
-        if (dropping) {
-            if (line.getHeight() < MaxLength) {
-                line.setHeight(line.getHeight() + Speed);
-            } else {
-                dropping = false;
-            }
-        } else if (goingUp) {
-            if (line.getHeight() > 0) {
-                line.setHeight(line.getHeight() - Speed);
-            } else {
-                goingUp = false;
-            }
+
+   // animate the line
+    public void update() {
+        if (dropping && length < MAX_LENGTH) {
+            length += SPEED;
+        } else if (dropping && length >= MAX_LENGTH) {
+            dropping = false;
+            goingUp = true;
         }
+
+        if (goingUp && length > 0) {
+            length -= SPEED;
+        }
+
+        if (length < 0) length = 0;
+
+        redrawLine();
     }
-}
+
+    // remove old rectangle and draw new one with updated length
+    private void redrawLine() {
+        canvas.remove(line);
+        line = new Rectangle(x, y, LINE_WIDTH, length);
+        line.setFillColor(java.awt.Color.BLACK);
+        canvas.add(line);
+    }
+
+    public Rectangle getShape() {
+        return line;
+    }
+    }
